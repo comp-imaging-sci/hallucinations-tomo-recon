6debac34cc70f00199c1dbc0a8f0237041281f8a
Sayantan Bhadra <sayantanbhadra83@gmail.com>
2021-03-18 18:52:55 -0500

    Added U-Net data

--- UNET/extract_recons.py
+++ UNET/extract_recons.py
@@ -4,7 +4,7 @@ import os
 import numpy as np 
 
 model = 'h_map'
-num_recons = 69
+num_recons = 5
 
 recon_dir = './experiments/'+model+'/reconstructions/'
 
@@ -16,45 +16,21 @@ if not os.path.exists(ensemble_dir_ood):
     os.makedirs(ensemble_dir_ood)
 
 # Ensemble of ind recons
-vol_count = 0
-img_count = 0
-
 print('Start saving ind recons\n')
-while vol_count < 50:
-    print('Vol = '+str(vol_count))
-    filename = recon_dir+'file_ind_'+str(vol_count)+'_pn_uni_0.3.h5'
-    recon = h5py.File(filename,'r')['reconstruction']
-    recon = np.asarray(recon)
-    for idx in range(min(10,recon.shape[0])):
-        if img_count == num_recons:
-            break
-        recon_slice = recon[idx]
-        np.save(ensemble_dir_ind+'recon_'+str(img_count)+'.npy',recon_slice)
-        print('Saving ind recon '+str(img_count))
-        img_count +=1
-    else:
-        vol_count +=1
-        continue
-    break
+filename = recon_dir+'file_ind.h5'
+recon = h5py.File(filename,'r')['reconstruction']
+recon = np.asarray(recon)
+for idx in range(num_recons):
+    recon_slice = recon[idx]
+    np.save(ensemble_dir_ind+'recon_'+str(idx)+'.npy',recon_slice)
 
-#Ensemble of ood recons
-vol_count = 0
-img_count = 0
-
+# Ensemble of ood recons
 print('Start saving ood recons\n')
-while vol_count < 50:
-    print('Vol = '+str(vol_count))
-    filename = recon_dir+'file_ood_'+str(vol_count)+'_pn_uni_0.3.h5'
-    recon = h5py.File(filename,'r')['reconstruction']
-    recon = np.asarray(recon)
-    for idx in range(recon.shape[0]):
-        if img_count == num_recons:
-            break
-        recon_slice = recon[idx]
-        np.save(ensemble_dir_ood+'recon_'+str(img_count)+'.npy',recon_slice)
-        print('Saving ood recon '+str(img_count))
-        img_count +=1
-    else:
-        vol_count +=1
-        continue
-    break
+filename = recon_dir+'file_ood.h5'
+recon = h5py.File(filename,'r')['reconstruction']
+recon = np.asarray(recon)
+for idx in range(num_recons):
+    recon_slice = recon[idx]
+    np.save(ensemble_dir_ood+'recon_'+str(idx)+'.npy',recon_slice)
+
+        
--- UNET/run_unet_test.sh
+++ UNET/run_unet_test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Change the checkpoint directory and file according to your final checkpoint
-python -u models/unet/test_unet.py --mode test --challenge singlecoil --data-path ./datasets/ --exp h_map --checkpoint ./experiments/h_map/version_0/checkpoints/epoch\=49.ckpt 
+python -u models/unet/test_unet.py --mode test --challenge singlecoil --data-path ./ --exp h_map --checkpoint ./experiments/h_map/epoch\=49.ckpt 
 
