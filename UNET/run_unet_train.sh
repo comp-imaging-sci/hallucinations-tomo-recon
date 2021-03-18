be8615327a5f76f3218b92e0aea8ea58786a0af8
Sayantan Bhadra <sayantanbhadra83@gmail.com>
2021-03-18 18:59:34 -0500

    Removed train launch script

--- UNET/run_unet_train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -u models/unet/train_unet.py --mode train --challenge singlecoil --data-path ./datasets/ --exp h_map --mask-type equispaced --gpus 1 --master_port=$RANDOM > train.log
