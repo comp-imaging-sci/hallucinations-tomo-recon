ae31d32ecbba0bc1013ea6b154395ec999608b55
Varun Ajit Kelkar <vak2@deeplearning.bioen.illinois.edu>
2021-03-30 00:26:32 -0500

    changed location of DIP save

--- DIP/run_dip_unet.sh
+++ DIP/run_dip_unet.sh
@@ -7,12 +7,11 @@ snr=20
 learning_rate=0.01
 date=210329
 reg_TV=1e-02
+T_in=1000
 
 gt_filename=../recon_data/gt_${Type}/gt_${idx}.npy
 meas_filename=../recon_data/kspace_${Type}/kspace_${idx}.npy
-results_dir=$basedir/phase_noise_experiments/recons_dip2_${Type}_pn_${phasenoise}/recon_$rootpath
-results_dir=./recons_${Type}/recon_${idx}
+mkdir -p ./recons_${Type}
+results_path=./recons_${Type}/recon_${idx}
 
-mkdir -p $results_dir
-
-python -u dip_main.py --snr $snr --learning_rate $learning_rate --reg_TV $reg_TV --date $date --gt_filename $gt_filename --meas_filename $meas_filename --results_dir $results_dir --net unet
+python -u dip_main.py --snr $snr --learning_rate $learning_rate --reg_TV $reg_TV --T_in $T_in --date $date --gt_filename $gt_filename --meas_filename $meas_filename --results_path $results_path --net unet
